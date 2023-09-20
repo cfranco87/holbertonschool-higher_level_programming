0ddc3b3f2ec0d9e1d1d76563ef2a8969f967ee4e
cfranco87 <carlosfrancoart@gmail.com>
2023-09-20 15:30:50 +0000

    qe

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter_qe in range(ord('a'), ord('z')+1):
     if letter_qe != ord('e') and letter_qe != ord('q'):
-        print(f"{letter_az:c}", end='')
+        print(f"{letter_qe:c}", end='')
