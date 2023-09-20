e3eaeacb618293db9579bdf76600383a2fd1ad4a
cfranco87 <carlosfrancoart@gmail.com>
2023-09-20 16:26:38 +0000

    alpha

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for letter_qe in range(ord('a'), ord('z')+1):
-    if letter_qe != ord('e') and letter_qe != ord('q'):
+for letter_qe in range(97, 123):
+    if letter_qe == 113 or letter_qe == 101:
+        continue
+    else:    
         print(f"{letter_qe:c}", end='')
