2fa5017d55a0133abd29ce0e901b500d3d6ae90b
cfranco87 <carlosfrancoart@gmail.com>
2023-09-20 00:07:33 +0000

    letters

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    print(chr(c), end='')
+for letter_az in range(ord('a'), ord('z')+1):
+    letter = chr(letter_az)
+    print(letter, end='')
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+for letter_qe in range(ord('a'), ord('z')+1):
+    letter = chr(letter_qe)
+    if letter not in "qe":
+        print(letter, end='')
