ffc71806310e793894616eb70c7df419733786fe
cfranco87 <carlosfrancoart@gmail.com>
2023-09-20 14:16:31 +0000

    b3tty

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for i in range (10):
+for i in range(10):
     for j in range(10):
-        if(i!=j):
-            if(j>i):
+        if (i != j):
+            if (j > i):
                 print(f"{i}{j}", end=", ")
 print(f"89")
