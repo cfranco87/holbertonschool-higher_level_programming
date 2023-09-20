52c8796606d2d1521acd25f75e08c74e57eb58d8
cfranco87 <carlosfrancoart@gmail.com>
2023-09-20 17:02:45 +0000

    s

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,5 @@ for letter_qe in range(97, 123):
     if letter_qe == 113 or letter_qe == 101:
         continue
     else:    
-        print(f"{letter_qe:c}", end='')
+        print("{:c}".format(letter_qe), end='')
+
