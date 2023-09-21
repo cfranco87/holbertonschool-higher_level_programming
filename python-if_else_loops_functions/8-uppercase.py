e57853445cfbd7c1ac254315a65cdfb3c2a8042b
cfranco87 <carlosfrancoart@gmail.com>
2023-09-21 18:22:25 +0000

    uppper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) >= ord('A') and ord(str) <= ord('Z'):
+    upper_str = ""
+    for letter in str:
+        chr_code = ord(letter)
+        if 97 <= chr_code <= 122:
+            upper_str += chr(chr_code - 32)
+        else:
+            upper_str += letter
+    return upper_str
