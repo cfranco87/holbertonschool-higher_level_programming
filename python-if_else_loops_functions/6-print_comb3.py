a9218038ef29e5fe2ee1ba015225f74d22a1edcc
cfranco87 <carlosfrancoart@gmail.com>
2023-09-21 15:08:37 +0000

    stuff

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+printed_89 = False
+
 for i in range(10):
     for j in range(10):
         if i != j and j > i:
             print("{0}{1}".format(i, j), end=", ")
-        if i == 79:
+        if i == 8 and not printed_89:
             print("{}".format(89), end="\n")
+            printed_89 = True 
+
