74e4f8087ff1a7e98b6c9c9e736c572d52558504
cfranco87 <carlosfrancoart@gmail.com>
2023-09-21 02:05:59 +0000

    ss

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+islower = __import__('7-islower').islower
+
 
 def islower(c):
 
     if ord(c) >= 97 and ord(c) <= 122:
-            return True
-        else:
-            return False
+        return True
+    else:
+        return False
