f1323dd3287fa2dde65adbc5be094c7f9bdb7128
cfranco87 <carlosfrancoart@gmail.com>
2023-09-21 14:32:53 +0000

    changes

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,5 @@ for i in range(10):
     for j in range(10):
         if i != j and j > i:
             print("{0}{1}".format(i, j), end=", ")
-print("{}".format(89), end="\n")
+        if i == 79:
+            print("{}".format(89), end="\n")
