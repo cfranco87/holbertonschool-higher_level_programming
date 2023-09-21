1aa2c3661305fce9e777be4a82d62f4facb7e6a9
cfranco87 <carlosfrancoart@gmail.com>
2023-09-21 02:16:13 +0000

    add

--- python-if_else_loops_functions/10-holder.py
+++
@@ -1,6 +0,0 @@
-for letter_az in range(ord('a'), ord('z')+1):
-    print(f"{letter_az:c}", end='')
-
-    ("{:c}".format(letter_az), end='')
-
-    print(f"{letter_az:c}", end='')
--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-islower = __import__('7-islower').islower
-
 
 def islower(c):
 
