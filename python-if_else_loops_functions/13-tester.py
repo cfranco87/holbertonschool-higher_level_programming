d16124f437a1b153bf85a4a5abbaafaeb65cffa3
cfranco87 <carlosfrancoart@gmail.com>
2023-09-21 18:43:22 +0000

    uppper

--- python-if_else_loops_functions/13-tester.py
+++ python-if_else_loops_functions/13-tester.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
-def uppercase(str):
-    upper_str = ""
+upper_str = ""
     for letter in str:
         chr_code = ord(letter)
         if 97 <= chr_code <= 122:
             upper_str += chr(chr_code - 32)
         else:
             upper_str += letter
-    print("{}".format(upper_str), end="\n")
+    
