f5945016350c91b9e5f845b6a69e09a31e7bc955
root <carlosfrancoart@gmail.com>
2023-09-23 20:49:33 +0000

    main

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
 
-from add_0 import add
-
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}\n".format(a, b, result), end="")
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}\n".format(a, b, result), end="")
