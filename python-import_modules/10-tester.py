1423e548bd8a6dc88043617db5c2db7a7db3d784
root <carlosfrancoart@gmail.com>
2023-09-24 14:46:22 +0000

    a

--- python-import_modules/10-tester.py
+++ python-import_modules/10-tester.py
@@ -2,3 +2,8 @@ a = 1
 b = 2
 result = add.add(a + b)
 print("{} + {} = {}\n".format(a, b, result), end="")
+
+    print("{} + {} = {}\n".format(a, b, result_add), end="")
+    print("{} - {} = {}\n".format(a, b, result_sub), end="")
+    print("{} * {} = {}\n".format(a, b, result_mul), end="")
+    print("{} / {} = {}\n".format(a, b, result_div), end="")
