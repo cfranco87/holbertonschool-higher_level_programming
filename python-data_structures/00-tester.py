46b57f257d277c2c6169bc52dc13cc3eaca76cd6
root <carlosfrancoart@gmail.com>
2023-09-25 18:07:40 +0000

    reverse

--- python-data_structures/00-tester.py
+++ python-data_structures/00-tester.py
@@ -1,4 +1,5 @@
 def print_reversed_list_integer(my_list=[]):
-    my_list = my_list[::-1]
-    for item in my_list
-        print("{:d}".format(item))
+    if my_list:
+        my_list.reverse()
+        for item in my_list
+            print("{:d}".format(item))
