aec155278340580bf34b41281c8db4a4a86ac8fb
root <carlosfrancoart@gmail.com>
2023-09-29 16:34:02 +0000

    floating

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = int(a // b)
+        result = a / b
     except ZeroDivisionError:
         result = None
     finally:
-        print("{} {.2f}".format("Inside result:", result))
-                
-   
+        print("{} {}".format("Inside result:", result))
+
+
