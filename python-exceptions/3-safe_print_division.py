cc07fbb1c352bf16bf72ae2dc660a6884a637870
root <carlosfrancoart@gmail.com>
2023-09-29 15:02:51 +0000

    wait

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    
+    try:
+        result = int(a // b)
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("{} {.2f}".format("Inside result:", result))
+                
+   
