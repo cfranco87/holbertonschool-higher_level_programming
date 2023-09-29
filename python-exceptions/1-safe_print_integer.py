bc8ce3a52afdf0834add65bd84b3b6ca1a0593ad
root <carlosfrancoart@gmail.com>
2023-09-29 02:56:52 +0000

    int

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    for x in my_list:
-        
- print("{\n}".format(x), end="")
- 
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value), end="")
+            print("")
+            return True
+        else:
+            return False
+    except ValueError:
+            return False
+
