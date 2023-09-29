7f0a75ca41710c6424021734bb5b9895ee4ec26d
root <carlosfrancoart@gmail.com>
2023-09-29 16:39:45 +0000

    result

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,9 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+        return result
     except ZeroDivisionError:
         result = None
     finally:
         print("{} {}".format("Inside result:", result))
-
-
