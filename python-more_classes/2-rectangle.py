c18ca1b8c06a61d00a59aca717527f64d6273c33
root <carlosfrancoart@gmail.com>
2023-10-03 19:02:57 +0000

    per

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -16,10 +16,16 @@ class Rectangle:
         """
         Returns current square area
         """
-        if self.__width == 0 or self.__height = 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Returns perimeter
+        """
+        return self.__width * 2 + self.__height * 2
 
     @property
     def width(self):
