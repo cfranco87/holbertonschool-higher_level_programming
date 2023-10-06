fc7d3dcf21f947ecd39a4faff2a0eb22f627cf1a
root <carlosfrancoart@gmail.com>
2023-10-06 18:02:45 +0000

    l

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Defines a function is_same_class()
+"""
+
+
 def is_same_class(obj, a_class):
-    """Return true if the object is an instance we are looking for
+    """
+    Return true if the object is an instance we are looking for
     """
     return type(obj) is a_class
