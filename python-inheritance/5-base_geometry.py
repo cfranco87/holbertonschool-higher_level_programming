14529285c12b21579f2a783f546afc4b233e065f
root <carlosfrancoart@gmail.com>
2023-10-07 00:34:32 +0000

    ll

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defining a BaseGeometry
+"""
+
+
+    class BaseGeometry:
+    """
+    Defines a BaseGeometry
+    """
+    def __init__(self):
+        """
+        Empty
+        """
+        pass
