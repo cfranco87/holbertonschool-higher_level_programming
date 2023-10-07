e13cc5ba14912771b8fbb356e074d725b28bec79
root <carlosfrancoart@gmail.com>
2023-10-07 01:09:51 +0000

    base

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defining a BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Defines a BaseGeometry
+    """
+
+    def area(self):
+        """
+        Returns area
+        """
+        raise Exception("area() is not implemented")
