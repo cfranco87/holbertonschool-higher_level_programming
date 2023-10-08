e24ce53504eb71e1d50bb452039504e40f40de87
root <carlosfrancoart@gmail.com>
2023-10-08 17:58:42 +0000

    heh

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     """
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print("{}".format(read_data), end="")
+        print(read_data, end='')
