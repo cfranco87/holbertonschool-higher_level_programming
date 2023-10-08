9db939ab6899687c1b1c975976e56a8a4ec3c16a
root <carlosfrancoart@gmail.com>
2023-10-08 19:57:35 +0000

    je

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
     Define how to write a file
     """
     with open(filename, "w", encoding="utf-8") as f:
-        print(f.read(), end="")
+        f.write(), end=""
