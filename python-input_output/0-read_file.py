fe8b83109cdeb0a235b7ecdc31b77bf8d96d27b2
root <carlosfrancoart@gmail.com>
2023-10-08 19:38:45 +0000

    hell

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,6 +8,5 @@ def read_file(filename=""):
     """
     Define how to read file
     """
-    if filename is not None and filename:
-        with open(filename, encoding="utf-8") as f:
-            print(f.read(), end="")
+    with open(filename, encoding="UTF-8") as f:
+        print(f.read(), end="")
