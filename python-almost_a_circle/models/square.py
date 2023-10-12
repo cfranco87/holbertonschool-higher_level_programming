a781c71ea8de812761d4f732bcd9bdd85364d554
root <carlosfrancoart@gmail.com>
2023-10-12 22:45:41 +0000

    11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,6 +35,30 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """
+        update to arguments
+        """
+        if args:
+            if len(args) >= 4:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            else:
+                if len(args) > 0:
+                    self.id = args[0]
+                if len(args) > 1:
+                    self.size = args[1]
+                if len(args) > 2:
+                    self.x = args[2]
+                if len(args) > 3:
+                    self.y = args[3]
+
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+
     def __str__(self):
         """
         Returns a string representation of the square.
