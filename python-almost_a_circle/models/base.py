f7b219efccaab478aa073533913383779e957eaa
root <carlosfrancoart@gmail.com>
2023-10-13 00:02:53 +0000

    14

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Creating Class Base
 """
+import json
 
 
 class Base:
@@ -16,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        convert a list of dictionaries to a JSON string
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
