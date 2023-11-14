9eb9e516dec2feb40956beda4084e593e6d0819f
root <carlosfrancoart@gmail.com>
2023-11-14 17:54:05 +0000

    show

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,4 @@
+SELECT User, Host FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SELECT User, Host FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
