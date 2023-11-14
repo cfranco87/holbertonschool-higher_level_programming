501c6e12ee3808dcc8547cc452b882b5fe661e2c
root <carlosfrancoart@gmail.com>
2023-11-14 18:35:21 +0000

    user 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- script 2
 -- creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost;
 SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
-GRANT SELECT on *.* TO user_0d_2@localhost;
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT on hbtn_0d_2.* TO user_0d_2@localhost;
+FLUSH PRIVILEGES;
