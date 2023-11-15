2aa2f1d340ae7c65d6c9d8e37442fe7289f24b1e
root <carlosfrancoart@gmail.com>
2023-11-15 15:18:06 +0000

    tv

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- database dump from hbtn_0d_tvshows to your MySQL server
-SELECT tv_shows.titles, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
