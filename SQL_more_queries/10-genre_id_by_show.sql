e67f86d2933126ed8cc213932cff08e7c90caf46
root <carlosfrancoart@gmail.com>
2023-11-15 15:14:58 +0000

    tv

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 USE hbtn_0d_tvshows;
 SELECT tv_shows.titles, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.genre_id;
+LEFT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.genre_id
 WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
