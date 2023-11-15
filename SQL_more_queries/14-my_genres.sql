7da3ee0702fe9e9fbe11b809f5080dac221d8353
root <carlosfrancoart@gmail.com>
2023-11-15 20:49:04 +0000

    dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,4 @@ SELECT tv_genres.name
 FROM tv_show_genres
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_show.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+ORDER BY tv_genres.name;
