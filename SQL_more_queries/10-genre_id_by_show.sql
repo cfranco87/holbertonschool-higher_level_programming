c6bb3ae030e5407135d448038be1e22d7d543b39
root <carlosfrancoart@gmail.com>
2023-11-15 15:13:38 +0000

    tv

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- database dump from hbtn_0d_tvshows to your MySQL server
 USE hbtn_0d_tvshows;
-
 SELECT tv_shows.titles, tv_show_genres.genre_id
-FROM 
-RIGHT JOIN cities ON cities.state_id = states.id;
+FROM tv_shows
+LEFT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.genre_id;
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
