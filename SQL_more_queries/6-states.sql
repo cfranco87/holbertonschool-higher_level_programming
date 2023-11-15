92f01ffc0d3ac47eb202dc9760dd8f993334dabf
root <carlosfrancoart@gmail.com>
2023-11-15 00:36:28 +0000

    states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- script 6
+--  that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT NOT NULL UNIQUE,
+    name VARCHAR(256) NOT NULL
+    PRIMARY KEY(id)
+);
