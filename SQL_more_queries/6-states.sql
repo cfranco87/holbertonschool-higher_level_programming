f4265430079507d4b82392c3cd5c2ba081b0385d
root <carlosfrancoart@gmail.com>
2023-11-15 00:50:23 +0000

    states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT NOT NULL UNIQUE,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id)
 );
