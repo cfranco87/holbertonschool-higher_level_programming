038ffd22f22456bcfd345ea73ff91072ea908c63
root <carlosfrancoart@gmail.com>
2023-11-15 04:00:09 +0000

    tv

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- database dump from hbtn_0d_tvshows to your MySQL server
 USE hbtn_0d_tvshows;
 
-SELECT cities.id, cities.name, states.name
-FROM states
+SELECT tv_shows.titles, tv_show_genres.genre_id
+FROM 
 RIGHT JOIN cities ON cities.state_id = states.id;
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- script that lists all cities contained in the database 
 SELECT cities.id, cities.name, states.name
 FROM states
-RIGHT JOIN cities ON cities.state_id = states.id;
+RIGHT JOIN tv_shows ON cities.state_id = states.id;
