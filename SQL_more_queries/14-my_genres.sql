4cce75fa746766fd9f5cc916479393a7c2cd70ae
root <carlosfrancoart@gmail.com>
2023-11-15 20:39:57 +0000

    dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,1 +1,6 @@
 -- lists all genres of the show Dexter.
+SELECT tv_genres.name 
+FROM tv_show
+LEFT JOIN tv_genres ON tv_show.genre_id = tv_genres.id
+WHERE tv_show.title = 'Dexter'
+ORDER BY tv_show_genres ASC;
