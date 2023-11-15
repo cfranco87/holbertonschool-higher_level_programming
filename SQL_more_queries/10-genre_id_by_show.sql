6cb5729154c0f7099d45e87c77a799f80df0a284
root <carlosfrancoart@gmail.com>
2023-11-15 15:16:31 +0000

    tv

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- database dump from hbtn_0d_tvshows to your MySQL server
-USE hbtn_0d_tvshows;
 SELECT tv_shows.titles, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.genre_id
