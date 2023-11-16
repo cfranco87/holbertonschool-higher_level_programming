1fa1ee3f268777e18c9319fab7d658c25eab0c42
root <carlosfrancoart@gmail.com>
2023-11-16 02:32:48 +0000

    comedy

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title
 FROM tv_show
 JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genre.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title;
