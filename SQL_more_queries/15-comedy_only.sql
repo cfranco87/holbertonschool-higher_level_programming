1044b81441d9a8ea16e64a586b1c0f2195e32e41
root <carlosfrancoart@gmail.com>
2023-11-16 01:48:34 +0000

    comedy

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,9 @@
 --  lists all Comedy shows in the database
+SELECT tv_genres.name 
+FROM tv_genres
+INNER JOIN tv_show_genres 
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title;
